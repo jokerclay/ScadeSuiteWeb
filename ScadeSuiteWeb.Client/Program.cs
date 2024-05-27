e9258950d48986f6f92c5dc042cb0843795b6502
jokerclay <2079209547@qq.com>
2024-05-27 22:38:04 +0800

    添加项目文件。

---
+++ ScadeSuiteWeb.Client/Program.cs
@@ -0,0 +1,16 @@
+using Microsoft.AspNetCore.Components.Web;
+using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+using Microsoft.FluentUI.AspNetCore.Components;
+using ScadeSuiteWeb.Client;
+
+var builder = WebAssemblyHostBuilder.CreateDefault(args);
+builder.RootComponents.Add<App>("#app");
+builder.RootComponents.Add<HeadOutlet>("head::after");
+
+builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+
+builder.Services.AddFluentUIComponents();
+builder.Services.AddHttpClient();
+
+
+await builder.Build().RunAsync();
---
+++ ScadeSuiteWeb.Shared/Class1.cs
@@ -0,0 +1,7 @@
+﻿namespace ScadeSuiteWeb.Shared
+{
+    public class Class1
+    {
+
+    }
+}
