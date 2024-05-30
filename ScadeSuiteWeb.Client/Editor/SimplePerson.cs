ce6e12c1efa066712dbbb6551fa3d17a184263d0
jokerclay <2079209547@qq.com>
2024-05-30 16:05:02 +0800

    fix UI

---
+++ ScadeSuiteWeb.Client/Editor/SimplePerson.cs
@@ -0,0 +1,8 @@
+﻿namespace ScadeSuiteWeb.Client.Editor;
+
+public class SimplePerson
+{
+        public string Firstname { get; set; }
+        public string Lastname { get; set; }
+        public int Age { get; set; }
+}
